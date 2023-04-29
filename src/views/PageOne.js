9a3b146c16c87941607555e7d304fddb1eb8c387
Katrina <kalynndier@gmail.com>
2023-04-28 23:51:29 -0400

    side notes

--- src/views/PageOne.js
+++ src/views/PageOne.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Main from '../modals/Main'
+import './pageone.scss'
 
 
 function PageOne() {
@@ -9,13 +10,21 @@ function PageOne() {
         title="PAGE ONE"
         description="MVC stands for Model View Controller and is used to separate business logic from presentation."
         secondPara="Model = Data | View = GUI | Controller = Brains"
-        thirdPara="The Model stores and manages data often in the form of a database. The View is the GUI which stands for Graphical User Interface and is the visual representation of the data given. The functionality given to the user (clicking a button or a link, etc) is contained in the View. The Controller connects the Model and View. It's the middle-man."
+        thirdPara="The MODEL stores and manages data often in the form of a database. The VIEW is the GUI which stands for Graphical User Interface and is the visual representation of the data given. The functionality given to the user (clicking a button or a link, etc) is contained in the View. The CONTROLLER connects the Model and View. It's the middle-man."
         link="https://hertimetocode.com"
         pageNumber="1"
         urlTwo = "/pagetwo"
         next = ">>"
         />
-        <p>SLIDE NOTES:</p>
+        <section className='notesSection'>
+          <p className='slideNotes'>SLIDE NOTES:</p>
+              <ul>
+                <li>One:</li>
+                <li>Two:</li>
+                <li>Three:</li>
+              </ul>
+        
+        </section>
 
  
     </>
